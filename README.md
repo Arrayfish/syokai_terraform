359607a04df988c7cf1193c3a6db4f65ed88e7ba
Arrayfish <uekusa_hideaki@yahoo.co.jp>
2024-05-15 19:26:52 +0900

    albを使用したec2インスタンスのauto scaling groupを作成

---
+++ README.md
@@ -0,0 +1,3 @@
+# 詳解Terraform 第3版　写本リポジトリ
+
+https://www.oreilly.co.jp/books/9784814400522/
--- ch2/main.tf
+++ ch2/main.tf
@@ -1,92 +1,153 @@
 provider "aws" {
-    region = "ap-northeast-1"
+  region = "ap-northeast-1"
 }
 
-# resource "aws_instance" "example" {
-#     ami = "ami-07c589821f2b353aa"
-#     instance_type = "t2.micro"
-#     subnet_id = aws_subnet.main.id
-#     vpc_security_group_ids = [aws_security_group.instance.id]
-#     user_data = <<-EOF
-#     #!/bin/bash
-#     echo "Hello, World!!!!!!!!" > index.html
-#     nohup busybox httpd -f -p ${var.server_port} & 
-#     EOF
+resource "aws_lb" "example" {
+  name               = "uekusa-lb-example"
+  load_balancer_type = "application"
+  subnets = tolist(data.aws_subnets.main.ids)
+  security_groups = [aws_security_group.alb.id]
+}
 
-#     user_data_replace_on_change = true
-
-#     tags={
-#         Name = "uekusa-terraform-example"
-#     }
-# }
-resource "aws_launch_template" "example" {
-    image_id = "ami-07c589821f2b353aa"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
-    user_data = base64encode(<<-EOF
-    #!/bin/bash
-    echo "Hello, World!!!!!!!!" > index.html
-    nohup busybox httpd -f -p ${var.server_port} & 
-    EOF
-    )
-    # Autoscaling Groupがある起動設定を使用する場合に必要
-    lifecycle {
-        create_before_destroy = true
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port = 80
+    protocol = "HTTP"
+    # デフォルトではシンプルな404ページを返す
+    default_action {
+        type = "fixed-response"
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code = "404"
+        }
     }
 }
 
-data "aws_vpc" "main" {
-    filter{
-        name = "tag:Name"
-        values = ["uekusa-example-vpc"]
-    }
-}
-data "aws_subnets" "main" {
-    filter {
-        name = "vpc-id"
-        values = [data.aws_vpc.main.id]
-    }
-}
+resource "aws_lb_target_group" "asg"{
+    name = "uekusa-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.main.id
 
-resource "aws_autoscaling_group" "example" {
-    launch_template {
-        id = aws_launch_template.example.id
-        version = "$Latest"
-    }
-    vpc_zone_identifier = data.aws_subnets.main.ids
-
-    min_size = 2
-    max_size = 10
-
-    tag{
-        key = "Name"
-        value = "uekusa-terraform-asg-example"
-        propagate_at_launch = true
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
     }
 }
 
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn = aws_lb_listener.http.arn
+    priority = 100
 
-resource "aws_security_group" "instance" {
-    name = "uekusa-example-instance"
+    condition{
+        path_pattern{
+            values = ["*"]
+        }
+    }
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.asg.arn
+    }
+}
+
+resource "aws_security_group" "alb" {
+    name = "uekusa-example-alb"
     vpc_id = aws_vpc.main.id
+    # allow inbound http requests
     ingress {
-        from_port = var.server_port
-        to_port = var.server_port
+        from_port = 80
+        to_port = 80
         protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # allow all outbound request
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
-variable "server_port"{
-    description = "The port the server will use for HTTP requests"
-    type = number
-    default = 8080
+
+resource "aws_launch_template" "example" {
+  image_id               = "ami-07c589821f2b353aa"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    echo "Hello, World!!!!!!!!" > index.html
+    nohup busybox httpd -f -p ${var.server_port} & 
+    EOF
+  )
+  # Autoscaling Groupがある起動設定を使用する場合に必要
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-# output "public_ip"{
-#     value = aws_instance.example.public_ip
-#     description = "The public IP address of the web server"
-# }
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = ["uekusa-example-vpc"]
+  }
+}
+data "aws_subnets" "main" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier = data.aws_subnets.main.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "uekusa-terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+
+resource "aws_security_group" "instance" {
+  name   = "uekusa-example-instance"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+output "alb_dns_name"{
+    value = aws_lb.example.dns_name
+    description = "The domain name of the load balancer"
+}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -95,30 +156,20 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "default" {
-    vpc_id = aws_vpc.main.id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.main.id
-    }
-}
 resource "aws_subnet" "main" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "ap-northeast-1a"
-    map_public_ip_on_launch = true
-    tags = {
-        Name = "uekusa-example-subnet"
-    }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  tags = {
+    Name = "uekusa-example-subnet"
+  }
 }
 
-resource "aws_route_table_association" "default" {
-    subnet_id      = aws_subnet.main.id
-    route_table_id = aws_route_table.default.id
+resource "aws_subnet" "sub" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-northeast-1c"
+  tags = {
+    Name = "uekusa-example-subnet2"
+  }
 }
-
