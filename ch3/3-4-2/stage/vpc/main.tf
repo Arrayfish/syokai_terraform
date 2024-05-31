c947f54893172c1a6d9ad376d1a6fffb60d8a610
Arrayfish <uekusa_hideaki@yahoo.co.jp>
2024-05-31 17:39:20 +0900

    vpcを作成してidを出力

--- ch3/3-4-2/stage/vpc/main.tf
+++ ch3/3-4-2/stage/vpc/main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+    region = "ap-northeast-1"
+}
 terraform {
   backend "s3" {
         bucket = "uekusa-terraform-up-and-running-state"
@@ -9,22 +12,27 @@ terraform {
     }
 }
 
-provider "aws" {
-    region = "ap-northeast-1"
-}
-
 resource "aws_vpc" "example" {
     cidr_block = "10.2.0.0/16"
     tags ={
         Name = "uekusa-example-vpc"
     }   
 }
-# DB用の複数AZにまたがるサブネットを作成
-resource "aws_subnet" "db_subnet" {
+
+# DB用の複数AZのサブネットを作成
+resource "aws_subnet" "db_subnet1" {
     vpc_id = aws_vpc.example.id
-    cidr_block = "10.2.0.1/24"
+    cidr_block = "10.2.1.0/24"
     availability_zone = "ap-northeast-1a"
     tags = {
-        Name = "uekusa-example-subnet"
+        Name = "uekusa-example-subnet1"
+    }
+}
+resource "aws_subnet" "db_subnet2" {
+    vpc_id = aws_vpc.example.id
+    cidr_block = "10.2.2.0/24"
+    availability_zone = "ap-northeast-1c"
+    tags = {
+        Name = "uekusa-example-subnet2"
     }
 }
