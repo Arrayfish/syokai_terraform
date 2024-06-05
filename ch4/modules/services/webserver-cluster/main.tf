542ce51c601b3ef5f083a9a4e94ecd2c4a9ce7c8
Arrayfish <uekusa_hideaki@yahoo.co.jp>
2024-06-05 14:43:14 +0900

    ローカル変数でマジックナンバー消す

--- ch4/modules/services/webserver-cluster/main.tf
+++ ch4/modules/services/webserver-cluster/main.tf
@@ -1,3 +1,11 @@
+locals {
+  http_port = 80
+  any_port = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips = ["0.0.0.0/0"]
+}
+
 resource "aws_lb" "example" {
   name               = "uekusa-${var.cluster_name}-lb-example"
   load_balancer_type = "application"
@@ -7,7 +15,7 @@ resource "aws_lb" "example" {
 
 resource "aws_lb_listener" "http" {
     load_balancer_arn = aws_lb.example.arn
-    port = 80
+    port = local.http_port
     protocol = "HTTP"
     # デフォルトではシンプルな404ページを返す
     default_action {
@@ -57,18 +65,18 @@ resource "aws_security_group" "alb" {
     vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
     # allow inbound http requests
     ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port = local.http_port
+        to_port = local.http_port
+        protocol = local.tcp_protocol
+        cidr_blocks = local.all_ips
     }
 
     # allow all outbound request
     egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port = local.any_port
+        to_port = local.any_port
+        protocol = local.any_protocol
+        cidr_blocks = local.all_ips
     }
 }
 
@@ -120,7 +128,7 @@ resource "aws_autoscaling_group" "example" {
     version = "$Latest"
   }
   vpc_zone_identifier = data.aws_subnets.main.ids
-  
+
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
@@ -141,7 +149,7 @@ resource "aws_security_group" "instance" {
   ingress {
     from_port   = var.server_port
     to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 }
