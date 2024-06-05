5db704326cfad261305b23d2b01c56860b6f2961
Arrayfish <uekusa_hideaki@yahoo.co.jp>
2024-06-05 18:39:44 +0900

    セキュリティグループのルールをインラインブロックから一つのリソースへ抽出した。

--- ch4/modules/services/webserver-cluster/main.tf
+++ ch4/modules/services/webserver-cluster/main.tf
@@ -63,21 +63,25 @@ resource "aws_lb_listener_rule" "asg" {
 resource "aws_security_group" "alb" {
     name = "uekusa-${var.cluster_name}-alb"
     vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
-    # allow inbound http requests
-    ingress {
-        from_port = local.http_port
-        to_port = local.http_port
-        protocol = local.tcp_protocol
-        cidr_blocks = local.all_ips
-    }
+}
 
-    # allow all outbound request
-    egress {
-        from_port = local.any_port
-        to_port = local.any_port
-        protocol = local.any_protocol
-        cidr_blocks = local.all_ips
-    }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "engress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port = local.any_port
+  to_port = local.any_port
+  protocol = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 data "terraform_remote_state" "db" {
@@ -94,7 +98,7 @@ resource "aws_launch_template" "example" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = base64encode(
-    templatefile("user-data.sh", {
+    templatefile("${path.module}/user-data.sh", {
       server_port = var.server_port
       db_address = data.terraform_remote_state.db.outputs.address
       db_port = data.terraform_remote_state.db.outputs.port
@@ -146,10 +150,14 @@ resource "aws_autoscaling_group" "example" {
 resource "aws_security_group" "instance" {
   name   = "uekusa-&{var.cluster_name}-alb"
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+}
+
+resource "aws_security_group_rule" "allow_8080_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.instance.id
+
+  from_port   = var.server_port
+  to_port     = var.server_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
 }
